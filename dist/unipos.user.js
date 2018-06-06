0f436203ee6dac0c717c84e5012d4232f120f0cf
Anh Tranngoc <manhhomienbienthuy@yahoo.com.vn>
2018-06-06 12:55:27 +0700

    Refactor code

--- dist/unipos.user.js
+++ dist/unipos.user.js
@@ -7,66 +7,66 @@
 // @run-at       document-idle
 // @grant        none
 // ==/UserScript==
-!function(n) {
-    var e = {};
+!function(e) {
+    var n = {};
     function t(o) {
-        if (e[o]) return e[o].exports;
-        var r = e[o] = {
+        if (n[o]) return n[o].exports;
+        var r = n[o] = {
             i: o,
             l: !1,
             exports: {}
         };
-        return n[o].call(r.exports, r, r.exports, t), r.l = !0, r.exports;
+        return e[o].call(r.exports, r, r.exports, t), r.l = !0, r.exports;
     }
-    t.m = n, t.c = e, t.d = function(n, e, o) {
-        t.o(n, e) || Object.defineProperty(n, e, {
+    t.m = e, t.c = n, t.d = function(e, n, o) {
+        t.o(e, n) || Object.defineProperty(e, n, {
             enumerable: !0,
             get: o
         });
-    }, t.r = function(n) {
-        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(n, Symbol.toStringTag, {
+    }, t.r = function(e) {
+        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
             value: "Module"
-        }), Object.defineProperty(n, "__esModule", {
+        }), Object.defineProperty(e, "__esModule", {
             value: !0
         });
-    }, t.t = function(n, e) {
-        if (1 & e && (n = t(n)), 8 & e) return n;
-        if (4 & e && "object" == typeof n && n && n.__esModule) return n;
+    }, t.t = function(e, n) {
+        if (1 & n && (e = t(e)), 8 & n) return e;
+        if (4 & n && "object" == typeof e && e && e.__esModule) return e;
         var o = Object.create(null);
         if (t.r(o), Object.defineProperty(o, "default", {
             enumerable: !0,
-            value: n
-        }), 2 & e && "string" != typeof n) for (var r in n) t.d(o, r, function(e) {
-            return n[e];
+            value: e
+        }), 2 & n && "string" != typeof e) for (var r in e) t.d(o, r, function(n) {
+            return e[n];
         }.bind(null, r));
         return o;
-    }, t.n = function(n) {
-        var e = n && n.__esModule ? function() {
-            return n.default;
+    }, t.n = function(e) {
+        var n = e && e.__esModule ? function() {
+            return e.default;
         } : function() {
-            return n;
+            return e;
         };
-        return t.d(e, "a", e), e;
-    }, t.o = function(n, e) {
-        return Object.prototype.hasOwnProperty.call(n, e);
+        return t.d(n, "a", n), n;
+    }, t.o = function(e, n) {
+        return Object.prototype.hasOwnProperty.call(e, n);
     }, t.p = "", t(t.s = 1);
-}([ function(n, e, t) {
+}([ function(e, n, t) {
     "use strict";
-    t.r(e);
+    t.r(n);
     const o = 5e5, r = "https://unipos.me/q/jsonrpc", i = new RegExp("https://unipos.me/.*?i=([a-f0-9-]+)");
     function c() {
-        const n = window.location.href.match(i);
-        return n && n[1];
+        const e = window.location.href.match(i);
+        return e && e[1];
     }
-    function a(n) {
-        n && n.parentNode.removeChild(n);
+    function a(e) {
+        e && e.parentNode.removeChild(e);
     }
-    function s(n = "received", e = "") {
+    function s(e = "received", n = "") {
         const t = c(), r = {
-            offset_card_id: e,
+            offset_card_id: n,
             count: o
         };
-        switch (n) {
+        switch (e) {
           case "received":
             r.to_member_id = t;
             break;
@@ -85,75 +85,69 @@
             params: r
         };
     }
-    async function u(n) {
-        const e = {
+    async function d(e) {
+        const n = {
             method: "post",
             headers: {
                 "Content-Type": "application/json",
                 "x-unipos-token": window.localStorage.getItem("authnToken")
             },
             cache: "no-cache",
-            body: JSON.stringify(n)
-        }, t = await fetch(r, e);
+            body: JSON.stringify(e)
+        }, t = await fetch(r, n);
         return (await t.json()).result;
     }
-    async function d(n = "received") {
-        let e, t = [], r = "";
+    async function u(e = "received") {
+        let n, t = [], r = "";
         do {
-            const o = s(n, r);
-            if (!(e = await u(o)) || !e.length) break;
-            t = t.concat(e), r = e[e.length - 1].id;
-        } while (e.length < o);
+            const o = s(e, r);
+            if (!(n = await d(o)) || !n.length) break;
+            t = t.concat(n), r = n[n.length - 1].id;
+        } while (n.length < o);
         return t;
     }
-    async function l(n) {
-        const e = {
+    async function l(e) {
+        const n = {
             received: 0,
             sent: 0,
             clapped: 0
-        }, [t, o, r] = n;
-        return t.forEach(n => {
-            e.received += n.point, e.received += n.praise_count;
-        }), o.forEach(n => {
-            e.received += n.praise_count, e.sent += n.point;
-        }), e.clapped = await async function(n) {
-            const e = c();
-            return (await Promise.all(n.map(async n => {
+        }, [t, o, r] = e;
+        return t.forEach(e => {
+            n.received += e.point, n.received += e.praise_count;
+        }), o.forEach(e => {
+            n.received += e.praise_count, n.sent += e.point;
+        }), n.clapped = await async function(e) {
+            const n = c();
+            return (await Promise.all(e.map(async e => {
                 const t = {
                     jsonrpc: "2.0",
                     method: "Unipos.GetCard2",
                     id: "Unipos.GetCard2",
                     params: {
-                        id: n.id
+                        id: e.id
                     }
                 };
-                return (await u(t)).praises.find(n => n.member.id === e).count;
-            }))).reduce((n, e) => n + e, 0);
-        }(r), e;
+                return (await d(t)).praises.find(e => e.member.id === n).count;
+            }))).reduce((e, n) => e + n, 0);
+        }(r), n;
     }
     async function p() {
-        const n = await Promise.all([ async function() {
-            return d("received");
-        }(), async function() {
-            return d("sent");
-        }(), async function() {
-            return d("clapped");
-        }() ]);
-        !function(n) {
-            let e = `\n        <div class="stats received">\n            <span class="highlight">${n.received}</span>\n            <h6>received</h6>\n        </div>\n        <div class="stats sent">\n            <span class="highlight">${n.sent}</span>\n            <h6>sent</h6>\n        </div>\n        <div class="stats clapped">\n            <span class="highlight">${n.clapped}</span>\n            <h6>clapped</h6>\n        </div>\n        <style>\n            #counter {\n                position: absolute;\n                top: 200px;\n                right: 10px;\n                width: 300px;\n                text-align: center;\n            }\n\n            #counter .stats {\n                float: left;\n                width: 33.33%;\n\n            }\n\n            #counter .highlight {\n                font-size: 30px;\n                font-weight: 700;\n            }\n\n\n            #counter .received {\n                color: #1e73be;\n            }\n            #counter .sent {\n                color: #be1eb9;\n            }\n            #counter .clapped {\n                color: #00f3ff;\n            }\n\n            #counter h6 {\n                margin: 0;\n                font-size: 14px;\n                font-weight: normal;\n                color: #4a4a4a;\n            }\n        </style>`;
+        const e = await Promise.all([ u("received"), u("sent"), u("clapped") ]);
+        !function(e) {
+            let n = `\n        <div class="stats received">\n            <span class="highlight">${e.received}</span>\n            <h6>received</h6>\n        </div>\n        <div class="stats sent">\n            <span class="highlight">${e.sent}</span>\n            <h6>sent</h6>\n        </div>\n        <div class="stats clapped">\n            <span class="highlight">${e.clapped}</span>\n            <h6>clapped</h6>\n        </div>\n        <style>\n            #counter {\n                position: absolute;\n                top: 200px;\n                right: 10px;\n                width: 300px;\n                text-align: center;\n            }\n\n            #counter .stats {\n                float: left;\n                width: 33.33%;\n\n            }\n\n            #counter .highlight {\n                font-size: 30px;\n                font-weight: 700;\n            }\n\n\n            #counter .received {\n                color: #1e73be;\n            }\n            #counter .sent {\n                color: #be1eb9;\n            }\n            #counter .clapped {\n                color: #00f3ff;\n            }\n\n            #counter h6 {\n                margin: 0;\n                font-size: 14px;\n                font-weight: normal;\n                color: #4a4a4a;\n            }\n        </style>`;
             const t = document.createElement("div");
-            t.setAttribute("id", "counter"), t.innerHTML = e, document.body.appendChild(t);
-        }(await l(n));
+            t.setAttribute("id", "counter"), t.innerHTML = n, document.body.appendChild(t);
+        }(await l(e));
     }
     let f = void 0, h = !1;
     new MutationObserver(async function() {
-        const n = c();
-        n ? h || f === n || (h = !0, a(document.getElementById("counter")), await p(), f = n, 
+        const e = c();
+        e ? h || f === e || (h = !0, a(document.getElementById("counter")), await p(), f = e, 
         h = !1) : a(document.getElementById("counter"));
     }).observe(document.body, {
         childList: !0,
         subtree: !0
     });
-}, function(n, e, t) {
-    n.exports = t(0);
+}, function(e, n, t) {
+    e.exports = t(0);
 } ]);
