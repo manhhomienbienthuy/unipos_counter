132e950723c6df2a7c5248c4aa9960bb7b52d059
Anh Tranngoc <manhhomienbienthuy@yahoo.com.vn>
2018-06-07 08:13:23 +0700

    Fix bugs when move to settings and other pages

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,14 @@
 import {getUserId, removeElement} from './lib/utils';
 import {counter} from './lib/unipos_counter';
 
-let currentUserId = undefined;
+let currentUserId = null;
 let isLoading = false;
 
 async function callback() {
     const newUserId = getUserId();
     if (!newUserId) {
         removeElement(document.getElementById('counter'));
+        currentUserId = null;
         return;
     }
 
